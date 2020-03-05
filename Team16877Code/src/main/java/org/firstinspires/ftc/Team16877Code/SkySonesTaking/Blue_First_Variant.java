46c8ec2b098645c894c2822cc55c718320787745
andreySm27 <35634515+andreySm27@users.noreply.github.com>
2020-03-05 11:00:27 +0200

    version 1.04

--- Team16877Code/src/main/java/org/firstinspires/ftc/Team16877Code/SkySonesTaking/Blue_First_Variant.java
+++ Team16877Code/src/main/java/org/firstinspires/ftc/Team16877Code/SkySonesTaking/Blue_First_Variant.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.Team16877Code.AutonautsAPI;
 
 import static java.lang.Math.abs;
 
-@Autonomous(name="BYXX")
+@Autonomous(name = "BYXX")
 public class Blue_First_Variant extends AutonautsAPI {
 
     @Override
@@ -20,33 +20,28 @@ public class Blue_First_Variant extends AutonautsAPI {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        while (opModeIsActive()) {
-            if(i){
-                liftServo(false);
-                runRight(0.40,0.85);
-                runForward(0.9,0.85);
-                liftServo(true);
-                sleep(200);
-                runBack(0.8,0.85);
-                runLeft(0.95,0.85);
-                liftServo(false);
-                sleep(200);
-                runBack(0.10,0.85);
+        while (!opModeIsActive()) ;
+        liftServo(false);
+        runRight(0.40, 0.85);
+        runForward(0.9, 0.85);
+        liftServo(true);
+        sleep(200);
+        runBack(0.8, 0.85);
+        runLeft(0.95, 0.85);
+        liftServo(false);
+        sleep(200);
+        runBack(0.10, 0.85);
 
-                runRight(1.65,0.85);
+        runRight(1.65, 0.85);
 
-                runForward(0.9, 0.85);
-                liftServo(true);
-                sleep(200);
-                runBack(0.8, 0.85);
-                runLeft(1.85, 0.85);
-                liftServo(false);
-                sleep(200);
-                runBack(0.10, 0.85);
-                runRight(0.35, 0.85);
-
-                i = !i;
-            }
-        }
+        runForward(0.9, 0.85);
+        liftServo(true);
+        sleep(200);
+        runBack(0.8, 0.85);
+        runLeft(1.85, 0.85);
+        liftServo(false);
+        sleep(200);
+        runBack(0.10, 0.85);
+        runRight(0.35, 0.85);
     }
 }
